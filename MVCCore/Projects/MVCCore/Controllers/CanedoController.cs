9a24649b933a1a0b1b7ffaca1ab6e36f7f964bcb
Flavio Canedo <flavio_canedo@live.com>
2018-03-08 23:40:37 -0800

    Created Actions using Routes

---
+++ MVCCore/Projects/MVCCore/Controllers/CanedoController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MVCCore.Controllers
+{
+    public class CanedoController : Controller
+    {
+        [Route("Canedo/Main-Page"), Route("", Order = 0)]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult IndexJson()
+        {
+            return Json("{'Nome':'Canedo'}");
+        }
+
+        public IActionResult RedirectToIndex()
+        {
+            return RedirectToAction("Index");
+        }
+
+        //Sample: {server}/Canedo/IndexParameter/1/Flavio/Canedo
+        [Route("Canedo/IndexParameter"), 
+            Route("Canedo/IndexParameter/{id:int}/{name:alpha}/{lastName:alpha}")]
+        public IActionResult IndexParameter(int id, string name, string lastName)
+        {
+            return
+                Json("{" + string.Format("'id':{0}, 'Name':{1}, 'LastName':{2}", id, name, lastName) + "}");
+        }
+
+        //Sample: {server}/Canedo/IndexDateTime/2018-03-08 10:41:55pm
+        [Route("Canedo/IndexDateTime"),
+            Route("Canedo/IndexDateTime/{dateTime:datetime}")]
+        public IActionResult IndexDateTime(DateTime dateTime)
+        {
+            return Json("{" + string.Format("'dateTime':{0}", dateTime) + "}");
+        }
+
+        //Sample: {server}/Canedo/IndexNumbers/123456789/49.99/1.234/1.234/123456789/CD2C1638-1638-72D5-1638-DEADBEEF1638
+        [Route("Canedo/IndexNumbers"),
+            Route("Canedo/IndexNumbers/{_int:int}/{_decimal:decimal}/{_double:double}/{_float:float}/{_long:long}/{guid:guid}")]
+        public IActionResult IndexNumbers(int _int, decimal _decimal, double _double, float _float, long _long, Guid guid)
+        {
+            return
+                Json("{" + 
+                string.Format(
+                    "'int':{0}, 'decimal':{1}, 'double':{2}, 'float':{3}, 'long':{4}, 'Guid':{5}", 
+                    _int, _decimal,_double, _float, _long, guid) + "}");
+        }
+
+        //Sample: {server}/Canedo/IndexLength/Canedo/Canedo/FlavioCanedo/FlavioCanedo
+        [Route("Canedo/IndexLength"),
+            Route("Canedo/IndexLength/{minLength:minlength(4)}/{maxLength:maxlength(6)}/{length:length(12)}/{range:length(8,16)}")]
+        public IActionResult IndexLength(string minLength, string maxLength, string length, string range)
+        {
+            return
+                Json("{" +
+                string.Format(
+                    "'minLength':{0}, 'maxLength':{1}, 'length':{2}, 'range':{3}",
+                    minLength, maxLength, length, range) + "}");
+        }
+
+        //Sample: {server]/Canedo/IndexLengthNumber/18/90/90
+        [Route("Canedo/IndexLengthNumber"),
+            Route("Canedo/IndexLengthNumber/{min:min(18)}/{max:max(120)}/{range:range(18,120)}")]
+        public IActionResult IndexLengthNumber(int min, int max, int range)
+        {
+            return Json("{" + string.Format("'min':{0}, 'max':{1}, 'range':{2}", min, max, range) + "}");
+        }
+
+        //Sample: {server}/Canedo/IndexCEP/17-055-100
+        [Route("Canedo/IndexCEP"),
+            Route("Canedo/IndexCEP/{cep:regex(^\\d{{2}}-\\d{{3}}-\\d{{3}}$)}")]
+        public IActionResult IndexCEP(string cep)
+        {
+            return Json("{" + string.Format("'cep':{0}", cep) + "}");
+        }
+    }
+}
