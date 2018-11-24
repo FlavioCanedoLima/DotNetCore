dbe921b74aa2a4f9e3d8b917bb0483746e9279d5
Flavio Canedo <flavio_canedo@live.com>
2018-11-24 12:45:20 -0200

    Treino

---
+++ Projects/MVCCoreEventos.IO/src/Eventos.IO.Domain/CommandHandlers/CommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using Eventos.IO.Domain.Interfaces;
+using FluentValidation.Results;
+using System;
+
+namespace Eventos.IO.Domain.CommandHandlers
+{
+    public abstract class CommandHandler
+    {
+        private readonly IUnityOfWork _unityOfWork;
+
+        public CommandHandler(IUnityOfWork unityOfWork)
+        {
+            _unityOfWork = unityOfWork;
+        }
+
+        protected void NotificarValidacoesErro(ValidationResult validationResult)
+        {
+            foreach (var error in validationResult.Errors)
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+        }
+
+        protected bool Commit()
+        {
+            var commandResponse = _unityOfWork.Commit();
+
+            if (commandResponse.Success) return true;
+
+            Console.WriteLine("Ocorreu um erro ao salvar os dados no banco");
+            return false;
+        }
+    }
+}
---
+++ Projects/MVCCoreEventos.IO/src/Eventos.IO.Domain/Eventos/CommandHandlers/EventoCommandHandler.cs
@@ -0,0 +1,51 @@
+﻿using Eventos.IO.Domain.CommandHandlers;
+using Eventos.IO.Domain.Core.Events;
+using Eventos.IO.Domain.Eventos.Commands;
+using Eventos.IO.Domain.Eventos.Repository;
+using Eventos.IO.Domain.Interfaces;
+using System;
+
+namespace Eventos.IO.Domain.Eventos.CommandHandlers
+{
+    public class EventoCommandHandler : CommandHandler, 
+        IHandler<RegistrarEventoCommand>, 
+        IHandler<AtualizarEventoCommand>, 
+        IHandler<ExcluirEventoCommand>
+    {
+        private readonly IEventoRepository _eventoRepository;
+
+        public EventoCommandHandler(IEventoRepository eventoRepository, IUnityOfWork unityOfWork)
+            : base(unityOfWork)
+        {
+            _eventoRepository = eventoRepository;
+        }
+
+        public void Handle(RegistrarEventoCommand message)
+        {
+            var evento = new Evento(message.Nome, message.DataInicio, message.DataFim, message.Gratuito, message.Valor, message.Online, message.NomeEmpresa);
+
+            if (!evento.IsValid())
+            {
+                NotificarValidacoesErro(evento.ValidationResult);
+                return;
+            }
+
+            _eventoRepository.Add(evento);
+
+            if (Commit())
+            {
+
+            }
+        }
+
+        public void Handle(AtualizarEventoCommand message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Handle(ExcluirEventoCommand message)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
